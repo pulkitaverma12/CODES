0d9ae674b3a254d3eb87479fbf354c6d706d17b8
pulkitaverma12 <pulkitaverma10@gmail.com>
2025-02-14 00:31:56 +0530

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,33 @@
+num1=[1,2,3,4,5]
+print(min(num1))
+print(max(num1))
+print(sum(num1))
+
+# sort
+num2=[67,45,23,21,12,12]
+num2.sort()
+print(num2)
+num2.pop()
+print(num2)
+
+# length of the array
+num3 = [1, 2, 3, 4, 5]  # Example list
+print(len(num3))  
+
+# concatenation
+print([1,2]+[4,5])
+
+# repeat
+print("hi" * 4)  
+
+# true or false
+print(4 in num3)
+print(8 in num3)
+
+# extend
+num3.extend([12,30,25,45])
+print(num3)
+
+# reverse
+num3.reverse()
+print(num3)
