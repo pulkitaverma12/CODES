79adca5cea00966cb1a305b3fa96f0647b7cfb1b
PULKITA VERMA <pulkitaverma10@gmail.com>
2025-03-05 19:58:37 +0530

    Create constructor.py

---
+++ constructor.py
@@ -0,0 +1,10 @@
+# def used for making an function
+# self is same as this keyword
+class person:
+    # __init__ it is used to make constructor
+    def __init__(self,name,age):   
+        self.name=name
+        self.age=age
+p1=person("priyal",18)
+print(p1.name)
+print(p1.age)
