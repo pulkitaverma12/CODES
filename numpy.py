6e119e604fc5c54edcd76c1e1937840b5e864ba8
PULKITA VERMA <pulkitaverma10@gmail.com>
2025-03-22 23:29:32 +0530

    Create numpy.py

---
+++ numpy.py
@@ -0,0 +1,16 @@
+import numpy as np
+# a=np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+# reshaped_a=a.reshape (6,2)
+# print(reshaped_a)
+
+# arr=np.array([[1,2],[3,4]])
+# flattened_arr=arr.flatten()
+# print(flattened_arr)
+
+# a=np.array([[1,2,3],[4,5,6]])
+# transposed_a=a.transpose()
+# print(transposed_a)
+
+a=np.arange(10)
+a.resize([3,6])
+print(a)
